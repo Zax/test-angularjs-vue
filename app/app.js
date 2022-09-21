d5af92e64dca47c062452f9160e00df929c8694c
Paolo Zacchi <paolo.zacchi@sintel.net>
2022-09-21 11:58:37 +0200

    Aggiunti 2 stati di navigazione

--- app/app.js
+++ app/app.js
@@ -8,18 +8,26 @@
         'ngSanitize',
       ])
     .config(function ($urlRouterProvider) {
-      // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
-      $urlRouterProvider.otherwise('/');
+      $urlRouterProvider.otherwise('/a');
     })
     .config(function ($stateProvider) {
       $stateProvider
-        .state('main', {
-          url: '/',
-          templateUrl: 'app/main.html',
-          controller: 'mainCtrl'
+        .state('statoA', {
+          url: '/a',
+          templateUrl: 'app/statoA.html',
+          controller: 'statoACtrl'
+        })
+        .state('statoB', {
+          url: '/b',
+          templateUrl: 'app/statoB.html',
+          controller: 'statoBCtrl'
         });
     })
-    .controller('mainCtrl', function($scope){
-      $scope.messaggio = 'Ciao!'
+    .controller('statoACtrl', function($scope){
+      $scope.messaggio = 'Ciao, sei in stato A!'
+    })
+    .controller('statoBCtrl', function($scope){
+      $scope.messaggio = 'Ciao, sei in stato B!'
     });
+    ;
 })(window.angular);
