ee3379307eea25d32b43bf17deb62f1f616f11e4
Paolo Zacchi <paolo.zacchi@sintel.net>
2022-09-21 12:24:14 +0200

    aggiunta direttiva  my-current-time

--- app/app.js
+++ app/app.js
@@ -27,7 +27,29 @@
       $scope.messaggio = 'Ciao, sei in stato A!'
     })
     .controller('statoBCtrl', function($scope){
-      $scope.messaggio = 'Ciao, sei in stato B!'
-    });
+      $scope.format = 'M/d/yy h:mm:ss a';
+    })
+    .directive('myCurrentTime', function($interval, dateFilter) {
+      function link(scope, element, attrs) {
+        var format,
+            timeoutId;
+        function updateTime() {
+          element.text(dateFilter(new Date(), format));
+        }
+        scope.$watch(attrs.myCurrentTime, function(value) {
+          format = value;
+          updateTime();
+        });
+        element.on('$destroy', function() {
+          $interval.cancel(timeoutId);
+        });
+        timeoutId = $interval(function() {
+          updateTime(); // update DOM
+        }, 1000);
+      }
+      return {
+        link: link
+      };
+    })
     ;
 })(window.angular);
